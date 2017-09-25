4956fa30aae23c1eaf904df7c28efb7d51de6eeb
vvwBai <447055767@qq.com>
2017-09-25 15:13:43 +0800

    联系人表添加了客户id和客户与联系人之间的关联

--- src/main/java/com/crm/biz/dynamic/service/impl/SysDynamicServiceImpl.java
+++ src/main/java/com/crm/biz/dynamic/service/impl/SysDynamicServiceImpl.java
@@ -1,9 +1,19 @@
 package com.crm.biz.dynamic.service.impl;
 
 import com.crm.biz.dynamic.service.ISysDynamicService;
+import com.crm.entity.SysDynamic;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/9/15.
  */
+@Service
 public class SysDynamicServiceImpl implements ISysDynamicService {
+
+    public List<SysDynamic> selectDynamicByUserId(){
+        return null;
+    }
+
 }
