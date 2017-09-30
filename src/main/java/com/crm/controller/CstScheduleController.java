016c9059555f9d59e6cd937208d867433c4fa4f5
tangyang96 <m13307278157@163.com>
2017-09-30 15:49:19 +0800

    机会模块

--- src/main/java/com/crm/controller/CstScheduleController.java
+++ src/main/java/com/crm/controller/CstScheduleController.java
@@ -2,13 +2,17 @@ package com.crm.controller;
 
 import com.crm.biz.customer.dao.CstRecordMapper;
 import com.crm.biz.customer.dao.CstScheduleMapper;
+import com.crm.biz.customer.service.ICstScheduleService;
 import com.crm.entity.CstRecord;
 import com.crm.entity.CstSchedule;
+
+import com.crm.utils.ObjectUtil;
 import com.crm.utils.TypeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -18,11 +22,29 @@ import java.util.Map;
 public class CstScheduleController {
     @Autowired
     private CstScheduleMapper cstScheduleMapper;
+
+    @Autowired
+    private ICstScheduleService iCstScheduleService;
+
     @RequestMapping("selectCstScheduleById")
     public Map selectById(){
         Map map= TypeUtil.successMap();
         CstSchedule cstSchedule=cstScheduleMapper.findById(1l);
         map.put("cstSchedule",cstSchedule);
         return map;
+    }
+
+    //按机会id查询进度表
+    @RequestMapping("/getCstSchedule")
+    public Map getCstSchedule(Long chId){
+        Map map= TypeUtil.successMap();
+       List<CstSchedule> cstScheduleList=iCstScheduleService.getCstSchedule(chId);
+        Boolean by= ObjectUtil.isNotNull(cstScheduleList);
+        if(by==true){
+            map.put("cstScheduleList",cstScheduleList);
+        }else {
+            map.put("cstScheduleList","查询失败，对象为空");
+        }
+        return  map;
     }
 }
