02bcd6a37463b706a009b936f7e4a57bd299b13e
tangyang96 <m13307278157@163.com>
2017-10-13 18:06:09 +0800

    进度

--- src/main/java/com/crm/biz/customer/service/impl/CstScheduleServiceImpl.java
+++ src/main/java/com/crm/biz/customer/service/impl/CstScheduleServiceImpl.java
@@ -1,7 +1,37 @@
 package com.crm.biz.customer.service.impl;
 
+import com.crm.biz.customer.dao.CstScheduleMapper;
+import com.crm.biz.customer.service.ICstScheduleService;
+import com.crm.entity.CstSchedule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/9/15.
  */
-public class CstScheduleServiceImpl {
+@Service
+public class CstScheduleServiceImpl implements ICstScheduleService{
+
+      @Autowired
+      private CstScheduleMapper cstScheduleMapper;
+
+    //根据机会id查询进度表
+    @Override
+    public List<CstSchedule> getCstSchedule(Long chId) {
+
+        return cstScheduleMapper.getCstSchedule(chId);
+    }
+
+    //根据机会id删除进度
+    @Override
+    public boolean deleteCstSchedule(Long chId) {
+
+        int count=cstScheduleMapper.deleteCstSchedule(chId);
+        if (count>0){
+           return true;
+        }
+        return false;
+    }
 }
