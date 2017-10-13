63048c03763aaf645879d78b8964310eed25d758
tangyang96 <m13307278157@163.com>
2017-10-13 17:55:12 +0800

    进度

--- src/main/java/com/crm/biz/customer/dao/CstScheduleMapper.java
+++ src/main/java/com/crm/biz/customer/dao/CstScheduleMapper.java
@@ -3,6 +3,7 @@ package com.crm.biz.customer.dao;
 import com.crm.entity.CstRecord;
 import com.crm.entity.CstSchedule;
 import org.apache.ibatis.annotations.Param;
+
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -12,6 +13,8 @@ import java.util.List;
  */
 @Component
 public interface CstScheduleMapper {
+
+
     CstSchedule findById(Long id);
 
     void addCstSchedule(CstSchedule cstSchedule);
@@ -19,4 +22,11 @@ public interface CstScheduleMapper {
     void deleteById(Long id);
 
     void updateCstScheduleById(CstSchedule cstSchedule);
+
+    //根据机会id查询进度表
+    List<CstSchedule> getCstSchedule(Long chId);
+
+    //根据机会id删除进度
+    int deleteCstSchedule(Long chId);
 }
+
--- src/main/java/com/crm/biz/customer/service/ICstScheduleService.java
+++ src/main/java/com/crm/biz/customer/service/ICstScheduleService.java
@@ -1,7 +1,15 @@
 package com.crm.biz.customer.service;
 
+import com.crm.entity.CstSchedule;
+
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/9/15.
  */
 public interface ICstScheduleService {
+
+    public List<CstSchedule> getCstSchedule(Long chId);
+
+    public  boolean deleteCstSchedule(Long chId);
 }
