65fb7542e5bb578db3c90981f80ac74c236561ba
project-room <2993460938@qq.com>
2017-10-16 10:37:03 +0800

    项目增加转换器、枚举、DTO

--- src/main/java/com/crm/common/BaseController.java
+++ src/main/java/com/crm/common/BaseController.java
@@ -1,10 +1,12 @@
 package com.crm.common;
 
+import com.crm.utils.TypeUtil;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2017/9/12.
@@ -19,6 +21,9 @@ public class BaseController {
         this.request = request;
         this.response = response;
         this.session = request.getSession();
+    }
+    protected Map<String, Object> result() {
+        return TypeUtil.successMap();
     }
 
 }
