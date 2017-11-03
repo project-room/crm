5856d0b441d74be018b103f667b2f63585d77888
project-room <2993460938@qq.com>
2017-11-03 15:54:04 +0800

    修复代码

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -13,26 +13,26 @@ $(function() {
 		$("#loginForm").submit();
     });
 
-	//下次自动登录
-	var userName=$("type[name='userName']").val();
-	if(userName==null){
-		location.href="/crm/cstCustomer/toLogin";
-	}else{
-        $.ajax({
-            url:"/crm/sysUser/remenberLogin",
-            data:{"userName":userName},
-            type: "POST",
-            success: function (data) {
-                if(data==true){
-                    alert("data.result:"+data.result);
-                    location.href="/crm/cstCustomer/toIndex"
-                }else{
-                    location.href="/crm/cstCustomer/toLogin"
-                }
-
-            }, fail: function (data) {
-            }
-        });
-	}
+	// //下次自动登录
+	// var userName=$("type[name='userName']").val();
+	// if(userName==null){
+	// 	// location.href="/crm/cstCustomer/toLogin";
+	// }else{
+     //    $.ajax({
+     //        url:"/crm/sysUser/remenberLogin",
+     //        data:{"userName":userName},
+     //        type: "POST",
+     //        success: function (data) {
+     //            if(data==true){
+     //                alert("data.result:"+data.result);
+     //                location.href="/crm/cstCustomer/toIndex"
+     //            }else{
+     //                location.href="/crm/cstCustomer/toLogin"
+     //            }
+    //
+     //        }, fail: function (data) {
+     //        }
+     //    });
+	// }
 
 });
